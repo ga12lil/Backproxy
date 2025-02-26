00a20dba56b4a809390db7fc8de288cb25a94623
ga12lil <agrigoren@sfedu.ru>
2025-02-26 22:10:33 +0300

    add logger (slf4j)

--- client/src/main/java/org/niisva/Main.java
+++ client/src/main/java/org/niisva/Main.java
@@ -1,7 +1,10 @@
 package org.niisva;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        log.info("hello");
     }
 }
