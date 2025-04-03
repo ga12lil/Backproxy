119f652fcacd615168c7d20420d1215e76759bb0
Artem <agrigoren@sfedu.ru>
2025-04-04 00:47:41 +0300

    docker build

--- client/src/main/java/org/niisva/Main.java
+++ client/src/main/java/org/niisva/Main.java
@@ -7,7 +7,7 @@ import org.niisva.client.WorkClient;
 public class Main {
     public static void main(String[] args) {
         try {
-            new Node("localhost", 8000).cmdClient.connect();
+            new Node(args[0], 8000).cmdClient.connect();
         } catch (Exception e) {
             log.info(e.getMessage());
         }
